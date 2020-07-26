f4911b6a7d3535639e4771e970e1f8aa8d75ac48
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2020-07-25 23:56:20 -0300

    Pagina Home feita

---
+++ pokemonreact/src/Paginas/App.js
@@ -0,0 +1,27 @@
+import React, { Component, Fragment } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import './App.css';
+import Header from '../Componentes/Header';
+import { Container, TypeContainer } from './Styles';
+import aquaimage from '../Imagens/unnamed.png'
+import logo from '../Imagens/logo_pokemon.png'
+import '../Imagens/Image.css'
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <Container>
+          <TypeContainer bgcolor="#00FFFF">
+          <img src={logo}></img>
+          </TypeContainer>
+        </Container>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
--- pokemonreact/src/tabela.js
+++ pokemonreact/src/tabela.js
@@ -1,53 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 
 const TableHead = () => {
-    return (
+    return(
         <thead>
-            <tr>
-                <th>Autores</th>
-                <th>preco</th>
-                <th>Preços</th>
-                <th>Comprar</th>
-            </tr>
+          <tr>
+            <th>Autor</th>
+            <th>Livro</th>
+            <th>Preco</th>
+            <th>Remover</th>
+          </tr>
         </thead>
     );
-};
-
-
-const TableBody = props => {
-    const linhas = props.pokemon.map((linha, index) => {
-        return (
-            <tr key={index}>
-                <td>{linha.nome}</td>
-                <td>{linha.preco}</td>
-                <td>{linha.tipo}</td>
-                <td><button>Comprar</button></td>
-            </tr>
-        )
-    });
-
-    return (
-        <tbody>
-            {linhas}
-        </tbody>
-    )
-
-
 }
 
-class Tabela extends Component {
+const TableBody = props =>{
+    
+    const linhas = props.autores.map((linha, index)=>{
+       return( 
+       <tr key={index}>
+            <td>{linha.nome}</td>
+            <td>{linha.livro}</td>
+            <td>{linha.preco}</td>
+            <td><button onClick={ () => props.removeAutor(index)} className="waves-effect waves-light indigo lighten-2 btn">Remover</button></td>
+        </tr>
+       );
+    });
 
-    render() {
-        const { pokemon } = this.props;
-        return (
-            <table>
-                <TableHead />
-                <TableBody pokemon={pokemon} />
-            </table>
+    return(
+        <tbody>
+          {linhas}
+        </tbody>
+    );
+}
+
+
+class Tabela extends Component{
+    
+
+    render(){
+        const { autores, removeAutor } = this.props;
+        
+        return(
+        <table className="centered highlight">
+        <TableHead />
+        <TableBody autores={autores} removeAutor = { removeAutor }/>
+        </table>
         );
     }
 
 }
-
 export default Tabela;
